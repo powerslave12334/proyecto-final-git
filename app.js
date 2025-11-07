58c21fb74f8187fa841819e195d6522cc26ce6ec
powerslave12334 <hideonlawea@gmail.com>
2025-11-07 16:06:26 -0600

    modificaciones de la rama principal en paralelo

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-function seconds (string){
-    for (let seg = 0; seg <= 59; seg++)
-    {
+function seconds(string) {
+    for (let seg = 0; seg <= 59; seg++) {
         console.log(`${string} ${seg} seg`);
     }
 }
-function minutes (string)
-{
-    for (let min = 0; min <= 59; min++)
-    {
+
+function minutes(string) {
+    for (let min = 0; min <= 59; min++) {
         seconds(`${string }${min} min`);
-    }   
+    }
 }
 
-function hours ()
-{
+function hours() {
     let string = "";
-    for (let hour = 0; hour <= 23; hour++)
-    {
+    for (let hour = 0; hour <= 23; hour++) {
         string = `${hour} hrs `
         minutes(string);
         string = "";
-    }    
+    }
 }
+
+hours();
